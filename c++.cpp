3dc537d65dca4873dfd2f85f94322f9fcb0b2355
hbayazid99 <52384470+hbayazid99@users.noreply.github.com>
2019-07-04 12:08:36 -0700

    Add files via upload

---
+++ c++.cpp
@@ -0,0 +1,15 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+
+   int n,a,b;
+   cin>>n;
+   for(int i=1;i<=n;i++)
+
+   {
+       cin>>a>>b;
+       cout<<"case "<<i<<": "<<a+b<<endl;
+   }
+    return 0;
+}
